665829863a583c4b402f5b7827bc6e4ee019df92
KateJ <>
2020-09-21 09:59:37 +0900

    first commit

---
+++ dist/precache-manifest.926cc852ff27e22f3be044d73009912e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa312a7d8157a9395773",
+    "url": "/css/app.a38ecd1b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "67d21e42ba2470c547eff07743464e5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa312a7d8157a9395773",
+    "url": "/js/app.f37fb553.js"
+  },
+  {
+    "revision": "4bec52cbb1279ef7a4a8",
+    "url": "/js/chunk-vendors.cf491c02.js"
+  },
+  {
+    "revision": "7ed14341796b2c8680810cb80ce9d522",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
